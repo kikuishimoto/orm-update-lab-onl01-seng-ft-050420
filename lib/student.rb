f146f99c7d1d5d9997bd4fe976d228281f50b2f5
kiku ishimoto <kikuish@gmail.com>
2020-06-04 20:20:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,18 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    if self.id
+      self.update
+    else 
+      sql = <<-SQL
+        INSERT INTO students (name, grade) VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end 
+  end 
+
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
